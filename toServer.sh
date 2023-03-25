318d57482a77bc72e650a8aa11450053f5d04c1c
Laurie Harding-Russell <laurieHardingRussell@gmail.com>
2023-03-25 07:46:58 -0600

    This is silly, but probably works

--- toServer.sh
+++ toServer.sh
@@ -8,10 +8,11 @@ scp $geoserver $sshCred:geoserver/geoserver.tar
 # scp $geoserver $sshCred:docker-compose.geoserver.yml 
 
 ssh $sshCred << EOF
+    docker stop geo
+    docker container rm geo
     docker image rm -f geoserver
     docker load -i geoserver/geoserver.tar
     echo "Loaded in the tar \n"
-    docker stop geo
-    docker run -itd --network=garden-land -p 6001:6001 --name geo geoserver
+    docker run -itd --network=garden-land -expose=6001:6001 --name geo geoserver
     echo "done! \n"
 EOF
